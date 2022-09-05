157b6982095b627a12ecaa2fca65da04bc922522
y2Dev <dub-y@tuta.io>
2022-09-05 11:44:21 -0400

    création et importation du composant Categories

--- Allapps/Ecommerce/Screens/Home.js
+++ Allapps/Ecommerce/Screens/Home.js
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.content}>
+      <Categories/>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    content: {
+        flex:1,
+        backgroundColor:"lightblue",
+    },
+})
