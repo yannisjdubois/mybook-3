ffa8e0d71c60c4ecb3ec3d819d7bcc85cc44bd5d
y2Dev <dub-y@tuta.io>
2022-09-05 11:22:32 -0400

    création d'une bottom navigation

--- Allapps/Ecommerce/index.js
+++ Allapps/Ecommerce/index.js
@@ -1,23 +1,41 @@
-import { View, Text } from 'react-native'
+// import { View, Text } from 'react-native'
 
-import React from 'react'
+// import React from 'react'
 
-import Connexion from './Public/Connexion';
+// import Connexion from './Public/Connexion';
 
-import Private from './Private';
+// import Private from './Private';
 
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-const Index = () => {
-    /* recupere la variable login présent dans le State */
-    const { login } = useSelector(state => state)
+// const Index = () => {
+//     /* recupere la variable login présent dans le State */
+//     const { login } = useSelector(state => state)
+//   return (
+//     <View>
+//       <Text>E-Commerce</Text>
+//       { !login ? <Connexion/>:
+//                 <Private/>}
+//     </View>
+//   )
+// }
+
+// export default Index
+
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Screens/Home';
+import Settings from './Screens/Settings';
+
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
-    <View>
-      <Text>E-Commerce</Text>
-      { !login ? <Connexion/>:
-                <Private/>}
-    </View>
-  )
+      <Tab.Navigator>
+        <Tab.Screen name="Accueil" component={Home} />
+        <Tab.Screen name="Mon Compte" component={Settings} />
+      </Tab.Navigator>
+  );
 }
-
-export default Index
